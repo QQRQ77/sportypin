63d3b7c8053adf6b128648c2659ed5d79065d3bd
QQRQ77 <tomaszkokot@o2.pl>
2025-10-07 19:38:08 +0200

    main page - redirect to events

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
-import { Button } from '@/components/ui/button'
-import { createUser } from '@/lib/users.actions';
-import { auth } from '@clerk/nextjs/server';
+// import { createUser } from '@/lib/users.actions';
+// import { auth } from '@clerk/nextjs/server';
+import {redirect} from 'next/navigation'
 
 const Page = async () => {
-  const session = await auth();
-  if ( session.userId ) {await createUser()}
+  redirect("/events");
+  // const session = await auth();
+  // if ( session.userId ) {await createUser()}
 
   return (
     <div className='mx-auto'>
       <h1 className='mx-auto text-2xl font-bold'>SportyPin</h1>
-      <p>Cały sport w jednym miejscu</p>
-      <p>Wkrótce więcej informacji!</p>
-      <Button>Zapisz się do newslettera</Button>
-      <div className='h-20'></div>
     </div>
   )
 }
