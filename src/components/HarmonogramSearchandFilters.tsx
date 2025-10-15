d822d3dd0f864bdafded57249caa0cbb43df0537
QQRQ77 <tomaszkokot@o2.pl>
2025-10-15 21:24:37 +0200

    event harmonogram - editing items with teams names- comments variebles

--- src/components/HarmonogramSearchandFilters.tsx
+++ src/components/HarmonogramSearchandFilters.tsx
@@ -1,20 +1,20 @@
 
 
-interface HarmonogramSearchAndFiltersProps {
-      setSearchString: (value: string) => void;
-      types: string[];
-      cathegories: string[];
-      teams: string[];
-      setFilterType: (value: string) => void;
-      setFilterCathegory: (value: string) => void;
-      setFilterTeam: (value: string) => void;
-}
+// interface HarmonogramSearchAndFiltersProps {
+//       setSearchString: (value: string) => void;
+//       types: string[];
+//       cathegories: string[];
+//       teams: string[];
+//       setFilterType: (value: string) => void;
+//       setFilterCathegory: (value: string) => void;
+//       setFilterTeam: (value: string) => void;
+// }
 
 // export default function HarmonogramSearchAndFilters({types, cathegories, teams, setSearchString, setFilterType, setFilterCathegory, setFilterTeam}: HarmonogramSearchAndFiltersProps ) {
 export default function HarmonogramSearchAndFilters() {
   return (
     <div className="w-full flex flex-wrap gap-2 mb-2 p-4 rounded-xl shadow-xl border-2">
-      Tutaj będzie w przyszłości opcja: SEARCH AND FILTERS
+      Tutaj będzie w przyszłości {"(niedługo)"} opcja: SEARCH AND FILTERS 
       {/* Tutaj dodaj komponenty do wyszukiwania i filtrowania */}
     </div>
   );
