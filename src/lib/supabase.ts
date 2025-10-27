7eedab6d0d3d58d101e694700910174c635fa855
QQRQ77 <tomaszkokot@o2.pl>
2025-10-27 21:13:33 +0100

    supabase client with service role

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,17 +1,30 @@
-// import { auth } from '@clerk/nextjs/server';
+// // import { auth } from '@clerk/nextjs/server';
+// import { createClient } from '@supabase/supabase-js';
+
+// export default function createSupabaseClient() {
+//   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+//   const supabaseAnonKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+//   if (!supabaseUrl || !supabaseAnonKey) {
+//     throw new Error('Missing Supabase URL or Anon Key in environment variables');
+//   }
+
+//   return createClient(
+//     supabaseUrl,
+//     supabaseAnonKey,
+//     // {async accessToken() {return ((await auth()).getToken())}});
+//   )
+// }
+
 import { createClient } from '@supabase/supabase-js';
 
 export default function createSupabaseClient() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const key = process.env.SUPABASE_SERVICE_ROLE_KEY; // ← poprawna nazwa
 
-  if (!supabaseUrl || !supabaseAnonKey) {
-    throw new Error('Missing Supabase URL or Anon Key in environment variables');
+  if (!url || !key) {
+    throw new Error('Missing Supabase URL or Service Role Key');
   }
 
-  return createClient(
-    supabaseUrl,
-    supabaseAnonKey,
-    // {async accessToken() {return ((await auth()).getToken())}});
-  )
+  return createClient(url, key);
 }
