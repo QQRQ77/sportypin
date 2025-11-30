32a624d3d65b21f00cffecc99987f8fb8b12087d
QQRQ77 <tomaszkokot@o2.pl>
2025-11-30 09:13:23 +0100

    Athlete page - bug with display - uuid problem

--- src/app/(site)/athlete/[athlete_Id]/page.tsx
+++ src/app/(site)/athlete/[athlete_Id]/page.tsx
@@ -1,28 +1,21 @@
-// import AthleteCard from "@/components/AthleteCard";
-// import { getAthleteById } from "@/lib/athletes.actions";
-import JsonViewer from "@/components/utils/JSONviewer";
+import AthleteCard from "@/components/AthleteCard";
+import { getAthleteById } from "@/lib/athletes.actions";
 import { createUser } from "@/lib/users.actions";
-// import { CreateAthlete } from "@/types";
+import { CreateAthlete } from "@/types";
 import { auth } from "@clerk/nextjs/server";
 
-export default async function AthletePage({ params }: { params: Promise<{ athlete_id: string }> }) {
+export default async function AthletePage({ params }: { params: Promise<{ athlete_Id: string }> }) {
 
-  const athlete_id = await params;
+  const { athlete_Id } = await params;
 
   const { userId } = await auth();
   if ( userId ) {await createUser()}
 
-  console.log("Athlete id: ", athlete_id);
-
-  // const athlete: CreateAthlete = await getAthleteById(athlete_id);
-
-  // console.log("Athlete data in AthletePage:", athlete);
+  const athlete: CreateAthlete = await getAthleteById(athlete_Id);
 
   return (
     <div className="flex items-center justify-center flex-col w-11/12 mx-auto mt-20 gap-4 mb-20">
-      Hello
-      <JsonViewer data={athlete_id} />
-      {/* <AthleteCard {...athlete} /> */}
+      <AthleteCard {...athlete} />
     </div>
   );
 }
