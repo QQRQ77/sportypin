23b539eb12260af593f134eb7e1dd02ae681c317
QQRQ77 <tomaszkokot@o2.pl>
2025-11-30 22:09:52 +0100

    Athlete page - home team Logo - problem with display

--- src/components/AthleteCard.tsx
+++ src/components/AthleteCard.tsx
@@ -21,7 +21,7 @@ export default function AthleteCard({ athlete, teamLogoURL }: AthleteCardProps) 
           height={500}
           className="object-contain"
         />
-        <div className="absolute bottom-0 w-full bg-orange-700 bg-opacity-60 text-white p-4 text-center flex flex-col justify-center items-center gap-3">
+        <div className="w-full bg-orange-700 bg-opacity-60 text-white p-4 text-center flex flex-col justify-center items-center gap-3">
           <h2 className="text-2xl font-bold">{athlete.first_name} {athlete.last_name}</h2>
           <h2 className="text-xl font-medium">{athlete.birth_year}</h2>
           <div className="flex justify-center items-center gap-4">
