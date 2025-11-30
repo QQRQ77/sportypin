15f69d4d478d2a1e12e56954275e3dd707a5f33b
QQRQ77 <tomaszkokot@o2.pl>
2025-11-30 21:50:06 +0100

    Athlete page - home team Logo - problem with display

--- src/app/(site)/athlete/[athlete_Id]/page.tsx
+++ src/app/(site)/athlete/[athlete_Id]/page.tsx
@@ -17,7 +17,7 @@ export default async function AthletePage({ params }: { params: Promise<{ athlet
 
   return (
     <div className="flex items-center justify-center flex-col w-11/12 mx-auto mt-20 gap-4 mb-20">
-      <AthleteCard athlete={athlete} teamLogoURL={teamLogoURL} />
+      <AthleteCard athlete={athlete} teamLogoURL={teamLogoURL || "/images/default_team_logo.jpeg"} />
     </div>
   );
 }
