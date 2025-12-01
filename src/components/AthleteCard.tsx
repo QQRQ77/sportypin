243b9537ce56d61f6a6a51aa7fbc4329e5e5882c
QQRQ77 <tomaszkokot@o2.pl>
2025-12-01 07:54:10 +0100

    Athlete page - home team Logo - problem with display

--- src/components/AthleteCard.tsx
+++ src/components/AthleteCard.tsx
@@ -15,23 +15,24 @@ export default function AthleteCard({ athlete }: AthleteCardProps) {
     <>
       <div className="h-[500px] border-4 border-orange-700 rounded-xl shadow-md flex flex-col items-center overflow-hidden bg-cyan-400 relative">
         <Image
-          src={athlete.imageUrls ? athlete.imageUrls[0] : "/images/athlete_avatar2.jpeg"}
-          alt={`${athlete.first_name} ${athlete.last_name ? athlete.last_name : ""}`}
-          width={384}
-          height={500}
-          className="object-contain"
-        />
+                src={athlete.home_team_logo_URL ? athlete.home_team_logo_URL[0] : "/images/default_team_logo.jpeg"}
+                alt={`${athlete.home_team_name} logo`}
+                width={400}
+                height={500}
+                className={`object-contain rounded-xl`}
+              />
         <div className="absolute bottom-0 w-full bg-orange-700 bg-opacity-60 text-white p-4 text-center flex flex-col justify-center items-center gap-3">
           <h2 className="text-2xl font-bold">{athlete.first_name} {athlete.last_name}</h2>
           <h2 className="text-xl font-medium">{athlete.birth_year}</h2>
           <div className="flex justify-center items-center gap-4">
+              
               <Image
-                src={athlete.home_team_logo_URL ? athlete.home_team_logo_URL[0] : "/images/default_team_logo.jpeg"}
-                alt={`${athlete.home_team_name} logo`}
-                width={50}
-                height={50}
-                className={`object-contain rounded-xl`}
-              />
+          src={athlete.imageUrls ? athlete.imageUrls[0] : "/images/athlete_avatar2.jpeg"}
+          alt={`${athlete.first_name} ${athlete.last_name ? athlete.last_name : ""}`}
+          width={50}
+          height={50}
+          className="object-contain"
+        />
             {athlete.home_team_id ?  
               <Link 
                 href={`/teams/${athlete.home_team_id}`}
