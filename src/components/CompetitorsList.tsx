f4803737a187f07b43804941388078e6d1d7a925
QQRQ77 <tomaszkokot@o2.pl>
2026-01-25 20:16:01 +0100

    logo display in participants list

--- src/components/CompetitorsList.tsx
+++ src/components/CompetitorsList.tsx
@@ -10,6 +10,7 @@ import CompetitorEditForm from "./forms/CompetitorEditForm";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 import { PencilSquareIcon, TrashIcon } from "@heroicons/react/20/solid";
 import { saveNewParticipant } from "@/lib/events.actions";
+import EventParticipantTypeTeam from "./teams/EventParticipantTypeTeam";
 
 interface CompetitorsProps {
   participants?: Participant[];
@@ -80,7 +81,7 @@ export default function CompetitorsList({eventId, setItems, participants = [], i
                         <div className="ml-5 font-medium text-lg">{participant.second_name}</div>
                       </div>
                     </>} 
-                    <div className="ml-5 font-medium text-lg">{participant.team_name}</div> 
+                    <EventParticipantTypeTeam participant={participant} />
                     {isUserCreator && 
                           <div className="flex flex-row w-24 justify-center items-center gap-4 ml-5">
                             <div className="text-gray-500 hover:text-gray-800">
