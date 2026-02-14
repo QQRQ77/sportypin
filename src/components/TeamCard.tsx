8476a8c5987fed8a3fc2e6e37004a191cf826222
QQRQ77 <tomaszkokot@o2.pl>
2026-02-14 16:39:25 +0100

    team card with logo from logoUrl

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -11,7 +11,7 @@ export default function TeamCard({ team }: TeamCardProps) {
     <>
       <div className="border-4 border-orange-700 rounded-xl shadow-md flex flex-col items-center overflow-hidden bg-cyan-400 relative">
         <Image
-          src={team.imageUrls && team.imageUrls[0] || "/images/default_team_logo.jpeg"}
+          src={team.logoUrl || "/images/default_team_logo.jpeg"}
           alt={`${team.name} logo`}
           width={400}
           height={400}
