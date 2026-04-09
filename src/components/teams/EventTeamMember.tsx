5789bac1479ddfdcfe6e19095c0048f93ae16659
QQRQ77 <tomaszkokot@o2.pl>
2026-04-09 20:02:38 +0200

    event team members list - styling - number ontop icon

--- src/components/teams/EventTeamMember.tsx
+++ src/components/teams/EventTeamMember.tsx
@@ -24,7 +24,7 @@ const EventTeamMember: React.FC<EventTeamMemberProps> = ({member}) => {
           <IconContext.Provider value={{ className: "text-sky-600" }}>
             <div className='relative'>
               <IoShirtOutline size={48} />
-              <p className="absolute top-0 left-0 transform -translate-x-1/2 -translate-y-1/2 font-bold">{member.start_number}</p>
+                <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold">{member.start_number}</p>
             </div>
           </IconContext.Provider>
           <div className="flex flex-row">
