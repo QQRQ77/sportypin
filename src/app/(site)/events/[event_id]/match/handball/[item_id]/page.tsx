c4e8e4e5a6009de4fb5565430cfd132d732c1395
QQRQ77 <tomaszkokot@o2.pl>
2026-04-15 19:51:48 +0200

    match handball page - chewron icon

--- src/app/(site)/events/[event_id]/match/handball/[item_id]/page.tsx
+++ src/app/(site)/events/[event_id]/match/handball/[item_id]/page.tsx
@@ -1,5 +1,6 @@
 import { getEventBaseInfo, getMatchInfo } from "@/lib/events.actions";
 import { HarmonogramItem } from "@/types";
+import { ChevronDoubleLeftIcon } from "@heroicons/react/20/solid";
 import Link from "next/link";
 
 export default async function HandballMatchPage({ params }: { params: Promise<{ event_id: string, item_id: string }> }) {
@@ -20,14 +21,15 @@ export default async function HandballMatchPage({ params }: { params: Promise<{ 
 
   return (
     <div className="min-h-screen bg-gray-50 p-8 w-full flex flex-col justify-center items-center mb-20">
-      <Link href={`/events/${event_id}`} className="text-blue-500 hover:underline w-42 border border-blue-500 rounded px-4 py-2 mb-6">
+      <Link href={`/events/${event_id}`} className="text-blue-500 hover:underline w-62 border border-blue-500 rounded px-4 py-2 mb-6">
+        <ChevronDoubleLeftIcon className="h-5 w-5" />
         Powrót do strony turnieju
       </Link>
-      <div className="max-w-4xl mx-auto">
-        <h1 className="text-4xl font-bold mb-8">{eventInfo.name}</h1>
-        <h1 className="text-3xl font-bold mb-8">{eventInfo.city}</h1>
+      <div className="max-w-3xl mx-auto flex flex-col items-center w-full gap-5">
+        <h1 className="text-3xl font-bold">{eventInfo.name}</h1>
+        <h1 className="text-3xl font-bold">{eventInfo.city}</h1>
         <h2>{new Date(eventInfo.start_date).toLocaleDateString('pl-PL', { year: 'numeric', month: 'long', day: '2-digit' })}</h2>
-        <h2> Mecz: {itemInfo ? `${itemInfo.team_1} vs ${itemInfo.team_2}` : ''}</h2>
+        <h1 className="text-4xl font-bold"> Mecz: {itemInfo ? `${itemInfo.team_1} vs ${itemInfo.team_2}` : ''}</h1>
       
       </div>
     </div>
