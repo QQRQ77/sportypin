b2b5f601312268c04e8017f6091677f93e2be2d5
QQRQ77 <tomaszkokot@o2.pl>
2026-04-15 07:07:21 +0200

    match handball page - link to event

--- src/app/(site)/events/[event_id]/match/handball/[item_id]/page.tsx
+++ src/app/(site)/events/[event_id]/match/handball/[item_id]/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 export default async function HandballMatchPage({ params }: { params: Promise<{ event_id: string, item_id: string }> }) {
 
@@ -5,6 +6,9 @@ export default async function HandballMatchPage({ params }: { params: Promise<{ 
 
   return (
     <div className="min-h-screen bg-gray-50 p-8">
+      <Link href={`/events/${event_id}`} className="text-blue-500 hover:underline ">
+        Powrót do strony turnieju
+      </Link>
       <div className="max-w-4xl mx-auto">
         <h1 className="text-4xl font-bold mb-8">Handball Match</h1>
         
