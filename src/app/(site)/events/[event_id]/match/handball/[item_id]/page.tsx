1802d7128666369189573b9c7f0eae0d2fc3df07
QQRQ77 <tomaszkokot@o2.pl>
2026-04-15 20:43:38 +0200

    match handball page - button - start and end time

--- src/app/(site)/events/[event_id]/match/handball/[item_id]/page.tsx
+++ src/app/(site)/events/[event_id]/match/handball/[item_id]/page.tsx
@@ -20,7 +20,7 @@ export default async function HandballMatchPage({ params }: { params: Promise<{ 
   console.log("Item info:", itemInfo);
 
   return (
-    <div className="min-h-screen bg-gray-50 w-full flex flex-col items-center mb-20 gap-5">
+    <div className="min-h-screen bg-gray-50 w-full flex flex-col items-center mt-5 mb-20 gap-5">
       <Link href={`/events/${event_id}`} className="text-blue-500 hover:underline w-66 border border-blue-500 rounded flex items-center gap-2 px-4 py-2 transition-colors">
         <ChevronDoubleLeftIcon className="h-5 w-5" />
         Powrót do strony turnieju
@@ -29,7 +29,7 @@ export default async function HandballMatchPage({ params }: { params: Promise<{ 
         <h1 className="text-3xl font-bold">{eventInfo.name}</h1>
         <h1 className="text-3xl font-bold">{eventInfo.city}</h1>
         <h2 className="text-2xl font-semibold">{new Date(eventInfo.start_date).toLocaleDateString('pl-PL', { year: 'numeric', month: 'long', day: '2-digit' })}</h2>
-        <h1 className="text-4xl font-bold"> Mecz: {itemInfo ? `${itemInfo.team_1} vs ${itemInfo.team_2}` : ''}</h1>
+        <h1 className="text-4xl font-bold"> Mecz nr: {itemInfo ? `${itemInfo.LP}` : ""} rozpoczęcie: {itemInfo ? itemInfo.start_time : ""} koniec: {itemInfo ? itemInfo.end_time : ""}</h1>
       
       </div>
     </div>
