f8c27ea7b5045d44fab04d49697f054541736b7c
QQRQ77 <tomaszkokot@o2.pl>
2026-04-16 21:36:00 +0200

    match handball page - timer - score board - v2

--- src/components/events/ScoreBoard.tsx
+++ src/components/events/ScoreBoard.tsx
@@ -14,18 +14,18 @@ const ScoreBoard: React.FC<ScoreBoardProps> = ({ team_1_score, team_2_score }) =
   
   return (
     <div className="scoreboard flex flex-2">
-      <div className="team-1 flex flex-col items-center gap-4">
+      <div className="team-1 w-32 flex flex-col items-center gap-4">
         <h2 className="text-6xl font-bold">{score1}</h2>
         <div className="flex gap-2">
-          <button onClick={() => setScore1(score1 + 1)} className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 cursor-pointer">+1</button>
-          <button onClick={() => setScore1(score1 - 1)} className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700 cursor-pointer">-1</button>
+          <button onClick={() => setScore1(score1 + 1)} className="px-4 py-2 bg-green-600 text-white text-3xl rounded hover:bg-green-700 cursor-pointer">+</button>
+          <button onClick={() => setScore1(score1 - 1)} className="px-4 py-2 bg-red-600 text-white text-3xl rounded hover:bg-red-700 cursor-pointer">-</button>
         </div>
       </div>
-      <div className="team-2 flex flex-col items-center gap-4">
+      <div className="team-2 w-32 flex flex-col items-center gap-4">
         <h2 className="text-6xl font-bold">{score2}</h2>
         <div className="flex gap-2">
-          <button onClick={() => setScore2(score2 + 1)} className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 cursor-pointer">+1</button>
-          <button onClick={() => setScore2(score2 - 1)} className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700 cursor-pointer">-1</button>
+          <button onClick={() => setScore2(score2 + 1)} className="px-4 py-2 bg-green-600 text-white text-3xl rounded hover:bg-green-700 cursor-pointer">+</button>
+          <button onClick={() => setScore2(score2 - 1)} className="px-4 py-2 bg-red-600 text-white text-3xl rounded hover:bg-red-700 cursor-pointer">-</button>
         </div>
       </div>
     </div>
