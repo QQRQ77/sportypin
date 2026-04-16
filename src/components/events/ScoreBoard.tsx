c38f9434f2f871cab5e3001daa70c6608a72c176
QQRQ77 <tomaszkokot@o2.pl>
2026-04-16 22:07:59 +0200

    match handball page - timer - score board - v6

--- src/components/events/ScoreBoard.tsx
+++ src/components/events/ScoreBoard.tsx
@@ -13,7 +13,7 @@ const ScoreBoard: React.FC<ScoreBoardProps> = ({ team_1_score, team_2_score }) =
   const [score2, setScore2] = useState(team_2_score);
   
   return (
-    <div className="scoreboard flex flex-2 gap-20">
+    <div className="scoreboard flex flex-3">
       <div className="team-1 w-52 flex flex-col items-center gap-4">
         <h2 className="text-9xl font-bold">{score1}</h2>
         <div className="flex gap-2">
@@ -21,6 +21,7 @@ const ScoreBoard: React.FC<ScoreBoardProps> = ({ team_1_score, team_2_score }) =
           <button onClick={() => setScore1(score1 - 1)} className="px-4 py-2 bg-red-600 text-white text-3xl rounded hover:bg-red-700 cursor-pointer">-</button>
         </div>
       </div>
+      <div className="text-center text-9xl font-bold w-16 lg:w-24">:</div>
       <div className="team-2 w-52 flex flex-col items-center gap-4">
         <h2 className="text-9xl font-bold">{score2}</h2>
         <div className="flex gap-2">
