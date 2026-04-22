f37eacfca2c75ac2371be4ca9d4e0030e5cc9180
QQRQ77 <tomaszkokot@o2.pl>
2026-04-22 21:11:52 +0200

    match handball page - team members - click logic - v8

--- src/components/events/teamsMembers.tsx
+++ src/components/events/teamsMembers.tsx
@@ -31,12 +31,12 @@ const MatchTeamsMembers: React.FC<TeamsMembersProps> = ({ team_1_members, team_2
 
   return (
     <div className='w-3/5 flex flex-2 items-center justify-center gap-8'>
-      <div className={`team-1 w-1/2 flex flex-col items-center gap-2 p-2 ${team1active && !noTeam1Members ? "border-5 border-blue-500 animate-pulse rounded-2xl" : ""}`} onClick={handleTeam1Click}>
+      <div className={`team-1 w-1/2 flex flex-col items-center gap-2 p-2 ${team1active && !noTeam1Members ? "border-5 border-blue-500 animate-pulse rounded-2xl" : ""}`}>
         {team_1_members && team_1_members.length > 0 ? (
-            <div className='grid grid-cols-2 md:grid-cols-3 gap-4 items-center justify-center cursor-pointer animate-none'
+            <div className='grid grid-cols-2 md:grid-cols-3 gap-4 items-center justify-center cursor-pointer'
               onClick={handleTeam1Click}>
             {team_1_members.map((member) => (
-              <div className={`flex flex-col items-center gap-2 ${team1active && !noTeam1Members ? "hover:bg-gray-300" :""}  rounded-2xl p-2`} key={member.id}>
+              <div className={`flex flex-col items-center gap-2 ${team1active && !noTeam1Members ? "hover:bg-gray-300" :""}  rounded-2xl p-2 opacity-100`} key={member.id}>
                 <IconContext.Provider value={{ className: "text-sky-600 hover:text-sky-800" }}>
                   <div className='relative'>
                     <IoShirtOutline size={96} />
@@ -51,7 +51,7 @@ const MatchTeamsMembers: React.FC<TeamsMembersProps> = ({ team_1_members, team_2
           <p>Brak członków zespołu 1</p>
         )}
       </div>
-      <div className={`team-2 w-1/2 flex flex-col items-center gap-2 p-2 ${team2active && !noTeam2Members ? "border-5 border-green-500 animate-ping rounded-2xl" : ""}`} onClick={handleTeam2Click}>
+      <div className={`team-2 w-1/2 flex flex-col items-center gap-2 p-2 ${team2active && !noTeam2Members ? "border-5 border-green-500 animate-pulse rounded-2xl" : ""}`}>
         {team_2_members && team_2_members.length > 0 ? (
           <div className='grid grid-cols-2 md:grid-cols-3 gap-4 items-center justify-center cursor-pointer'
             onClick={handleTeam2Click}
