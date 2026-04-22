5e6873ebba31699a3f1d92e0cdb5c0b29e69f14a
QQRQ77 <tomaszkokot@o2.pl>
2026-04-22 20:56:05 +0200

    match handball page - team members - click logic - v4

--- src/components/events/teamsMembers.tsx
+++ src/components/events/teamsMembers.tsx
@@ -31,22 +31,22 @@ const MatchTeamsMembers: React.FC<TeamsMembersProps> = ({ team_1_members, team_2
 
   return (
     <div className='w-3/5 flex flex-2 items-center justify-center gap-8'>
-      <div className={`team-1 w-1/2 flex flex-col items-center gap-2 p-2 ${team1active && !noTeam1Members ? "border-5 border-blue-500 rounded-2xl animate-border-pulse" : ""}`} onClick={handleTeam1Click}>
+      <div className={`team-1 w-1/2 flex flex-col items-center gap-2 p-2 ${team1active && !noTeam1Members ? "border-5 border-blue-500 animate-pulse rounded-2xl" : ""}`} onClick={handleTeam1Click}>
         {team_1_members && team_1_members.length > 0 ? (
-        <div className='grid grid-cols-2 md:grid-cols-3 gap-4 items-center justify-center cursor-pointer'
-          onClick={handleTeam1Click}>
-        {team_1_members.map((member) => (
-          <div className={`flex flex-col items-center gap-2 ${team1active && !noTeam1Members ? "hover:bg-gray-300" :""}  rounded-2xl p-2`} key={member.id}>
-            <IconContext.Provider value={{ className: "text-sky-600 hover:text-sky-800" }}>
-          <div className='relative'>
-            <IoShirtOutline size={96} />
-            <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold text-4xl hover:text-gray-600">{member.start_number}</p>
-          </div>
-            </IconContext.Provider>
-            <p className="text-base font-bold text-wrap">{member.name}</p>
-          </div>
-        ))}
-        </div>
+            <div className='grid grid-cols-2 md:grid-cols-3 gap-4 items-center justify-center cursor-pointer'
+              onClick={handleTeam1Click}>
+            {team_1_members.map((member) => (
+              <div className={`flex flex-col items-center gap-2 ${team1active && !noTeam1Members ? "hover:bg-gray-300" :""}  rounded-2xl p-2`} key={member.id}>
+                <IconContext.Provider value={{ className: "text-sky-600 hover:text-sky-800" }}>
+                  <div className='relative'>
+                    <IoShirtOutline size={96} />
+                    <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold text-4xl hover:text-gray-600">{member.start_number}</p>
+                  </div>
+                </IconContext.Provider>
+                <p className="text-base font-bold text-wrap">{member.name}</p>
+              </div>
+            ))}
+            </div>
         ) : (
           <p>Brak członków zespołu 1</p>
         )}
