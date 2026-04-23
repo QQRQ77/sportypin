bbf880bf292f93a4109a6a91928a89cc0a2b7876
QQRQ77 <tomaszkokot@o2.pl>
2026-04-23 19:55:29 +0200

    match handball page - team members - active members after score - logic - v6

--- src/components/events/ScoreBoard.tsx
+++ src/components/events/ScoreBoard.tsx
@@ -17,7 +17,7 @@ interface ScoreBoardProps {
 }
 
 const ScoreBoard: React.FC<ScoreBoardProps> = ({ 
-  team_1_score, team_2_score, isUserCreator = false, team1active = true, team2active = true, members1active = true, 
+  team_1_score, team_2_score, isUserCreator = false, team1active = true, team2active = true, members1active = true, members2active = true,
   setTeam1Active, setTeam2Active, setMembers1Active, setMembers2Active }) => {
 
   const [score1, setScore1] = useState(team_1_score);
@@ -59,7 +59,7 @@ const ScoreBoard: React.FC<ScoreBoardProps> = ({
 
 
   return (
-    <div className="scoreboard flex flex-3">
+    <div className="scoreboard flex flex-3 border-1 border-gray-300 rounded-xl">
       <div className="team-1 w-52 flex flex-col items-center gap-4">
         <h2 className="text-9xl font-bold">{score1}</h2>
         {isUserCreator && 
@@ -73,7 +73,7 @@ const ScoreBoard: React.FC<ScoreBoardProps> = ({
         <h2 className="text-9xl font-bold">{score2}</h2>
         {isUserCreator && 
           <div className="flex gap-2">
-            <button onClick={handleTeam2ClickAdd} className={`px-4 py-2 ${team2active ? 'bg-green-600 hover:bg-green-700' : 'bg-gray-400'} text-white text-3xl rounded cursor-pointer`}>+</button>
+            <button onClick={handleTeam2ClickAdd} className={`px-4 py-2 ${team2active && !members2active ? 'bg-green-600 hover:bg-green-700' : 'bg-gray-400'} text-white text-3xl rounded cursor-pointer`}>+</button>
             <button onClick={handleTeam2ClickSub} className={`px-4 py-2 ${team2active ? 'bg-red-600 hover:bg-red-700' : 'bg-gray-400'} text-white text-3xl rounded cursor-pointer`}>-</button>
           </div>}
       </div>
