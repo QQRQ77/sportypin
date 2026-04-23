64dab95abc63d99f4408db90cc2c80b4651caf7c
QQRQ77 <tomaszkokot@o2.pl>
2026-04-23 19:50:07 +0200

    match handball page - team members - active members after score - logic - v5

--- src/components/events/teamsMembers.tsx
+++ src/components/events/teamsMembers.tsx
@@ -14,34 +14,39 @@ interface TeamsMembersProps {
   members2active: boolean;
   setTeam1Active: (active: boolean) => void;
   setTeam2Active: (active: boolean) => void;
-  setMembers1Active?: (active: boolean) => void;
-  setMembers2Active?: (active: boolean) => void;
+  setMembers1Active: (active: boolean) => void;
+  setMembers2Active: (active: boolean) => void;
 }
 
 const MatchTeamsMembers: React.FC<TeamsMembersProps> = 
-  ({ team_1_members, team_2_members, members1active = true, members2active = true, setTeam1Active, setTeam2Active }) => {
+  ({ team_1_members, team_2_members, members1active = true, members2active = true, 
+    setTeam1Active, setTeam2Active, setMembers1Active, setMembers2Active }) => {
 
   const noTeam1Members = !team_1_members || team_1_members.length === 0;
   const noTeam2Members = !team_2_members || team_2_members.length === 0;
 
   const handleTeam1Click = () => {
     setTeam1Active(true);
-    setTeam2Active(false);
+    setTeam2Active(true);
+    setMembers1Active(false);
+    setMembers2Active(false);
   }
 
   const handleTeam2Click = () => {
-    setTeam1Active(false);
+    setTeam1Active(true);
     setTeam2Active(true);
+    setMembers1Active(false);
+    setMembers2Active(false);
   }
 
   return (
     <div className='w-3/5 flex flex-2 items-center justify-center gap-8'>
       <div className={`team-1 w-1/2 flex flex-col items-center gap-2 p-2 ${members1active && !noTeam1Members ? "pulse-border-blue rounded-2xl" : "border-5 border-transparent"}`}>
         {team_1_members && team_1_members.length > 0 ? (
-            <div className='grid grid-cols-2 md:grid-cols-3 gap-4 items-center justify-center cursor-pointer'
-              onClick={handleTeam1Click}>
+            <div className='grid grid-cols-2 md:grid-cols-3 gap-4 items-center justify-center cursor-pointer'>
             {team_1_members.map((member) => (
-              <div className={`flex flex-col items-center gap-2 ${members1active && !noTeam1Members ? "hover:bg-gray-300" :""}  rounded-2xl p-2`} key={member.id}>
+              <div className={`flex flex-col items-center gap-2 ${members1active && !noTeam1Members ? "hover:bg-gray-300" :""}  rounded-2xl p-2`} key={member.id}
+                onClick={handleTeam1Click}>
                 <IconContext.Provider value={{ className: "text-sky-600 hover:text-sky-800" }}>
                   <div className='relative'>
                     <IoShirtOutline size={96} />
@@ -58,11 +63,10 @@ const MatchTeamsMembers: React.FC<TeamsMembersProps> =
       </div>
       <div className={`team-2 w-1/2 flex flex-col items-center gap-2 p-2 ${members2active && !noTeam2Members ? "pulse-border-green rounded-2xl" : "border-5 border-transparent"}`}>
         {team_2_members && team_2_members.length > 0 ? (
-          <div className='grid grid-cols-2 md:grid-cols-3 gap-4 items-center justify-center cursor-pointer'
-            onClick={handleTeam2Click}
-          >
+          <div className='grid grid-cols-2 md:grid-cols-3 gap-4 items-center justify-center cursor-pointer'>
             {team_2_members.map((member) => (
-              <div className={`flex flex-col items-center gap-2 ${members2active && !noTeam2Members ? "hover:bg-gray-300" :""} rounded-2xl p-2`} key={member.id}>
+              <div className={`flex flex-col items-center gap-2 ${members2active && !noTeam2Members ? "hover:bg-gray-300" :""} rounded-2xl p-2`} key={member.id}
+                onClick={handleTeam2Click}>
               <IconContext.Provider value={{ className: "text-green-600 hover:text-green-800" }}>
                 <div className='relative cursor-pointer'>
                   <IoShirtOutline size={96} />
