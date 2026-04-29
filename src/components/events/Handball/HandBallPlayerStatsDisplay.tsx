53d2ad47ee522cb2b0334186e4d700bb563f0871
QQRQ77 <tomaszkokot@o2.pl>
2026-04-29 21:25:16 +0200

    match handball page - players stats display - icon and digits - v3

--- src/components/events/Handball/HandBallPlayerStatsDisplay.tsx
+++ src/components/events/Handball/HandBallPlayerStatsDisplay.tsx
@@ -18,11 +18,11 @@ const HandBallPlayerStatsDisplay: React.FC<HandBallPlayerStatsDisplayProps> = ({
 
 }) => {
   return (
-    <div className='w-18 flex flex-col items-center justify-start gap-2 border'>
-      {goals > 0 && <div className='w-full flex flex-2 items-center gap-1'><div className='w-8 text-center'><PiSoccerBallLight size={24} /></div><div className='w-10 text-green-700 font-bold'>{goals}</div></div>}
-      {penalties > 0 && <div className='flex flex-2 items-center gap-1'><div className='w-8 text-center'><PiNumberTwoFill size={24} /></div><div className='w-10 font-bold'>{penalties}</div></div>}
-      {yellowCards > 0 && <div className='flex flex-2 items-center justify-center gap-1'><div className='w-8 text-center'><div className="w-4 h-8 bg-yellow-300 rounded"></div></div><div className="w-10 font-bold">{yellowCards}</div></div>}
-      {redCards > 0 && <div className='flex flex-2 items-center justify-center gap-1'><div className='w-8 text-center'><div className="w-4 h-8 bg-red-500 rounded"></div></div><div className='w-10 font-bold'>{redCards}</div></div>}
+    <div className='w-16 flex flex-col items-center justify-start gap-2'>
+      {goals > 0 && <div className='w-full flex flex-2 items-center gap-1'><div className='w-8 flex justify-center items-center'><PiSoccerBallLight size={24} /></div><div className='w-8 text-green-700 font-bold'>{goals}</div></div>}
+      {penalties > 0 && <div className='flex flex-2 items-center gap-1'><div className='w-8 flex justify-center items-center'><PiNumberTwoFill size={24} /></div><div className='w-8 font-bold'>{penalties}</div></div>}
+      {yellowCards > 0 && <div className='flex flex-2 items-center justify-center gap-1'><div className='w-8 flex justify-center items-center'><div className="w-4 h-8 bg-yellow-300 rounded"></div></div><div className="w-8 font-bold">{yellowCards}</div></div>}
+      {redCards > 0 && <div className='flex flex-2 items-center justify-center gap-1'><div className='w-8 flex justify-center items-center'><div className="w-4 h-8 bg-red-500 rounded"></div></div><div className='w-8 font-bold'>{redCards}</div></div>}
     </div>
   );
 };
