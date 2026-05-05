410671f2f554a1ca80eedc8df0838f9fcd0b933d
QQRQ77 <tomaszkokot@o2.pl>
2026-05-05 21:31:59 +0200

    match handball page - score board - justify-start

--- src/components/events/teamsMembers.tsx
+++ src/components/events/teamsMembers.tsx
@@ -59,9 +59,9 @@ const MatchTeamsMembers: React.FC<TeamsMembersProps> =
 
   return (
     <div className='w-full lg:w-3/5 flex flex-2 items-center justify-center gap-8'>
-      <div className={`team-1 w-1/2 flex flex-col items-center gap-2 p-2 ${members1active && !noTeam1Members ? "pulse-border-blue rounded-2xl" : "border-5 border-transparent"}`}>
+      <div className={`team-1 w-1/2 flex flex-col items-center justify-start gap-2 p-2 ${members1active && !noTeam1Members ? "pulse-border-blue rounded-2xl" : "border-5 border-transparent"}`}>
         {team_1_members && team_1_members.length > 0 ? (
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-4 items-center justify-start'>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-4 items-center justify-center'>
             {team_1_members.map((member) => (
               <div className="flex flex-2"  key={member.id}>
                 <div className={`flex flex-col items-center justify-center gap-2 ${members1active ? "hover:bg-gray-300 cursor-pointer" :""}  rounded-2xl p-2`}
@@ -87,9 +87,9 @@ const MatchTeamsMembers: React.FC<TeamsMembersProps> =
           <p>Brak danych o zawodnikach</p>
         )}
       </div>
-      <div className={`team-2 w-1/2 flex flex-col items-center gap-2 p-2 ${members2active && !noTeam2Members ? "pulse-border-green rounded-2xl" : "border-5 border-transparent"}`}>
+      <div className={`team-2 w-1/2 flex flex-col items-center justify-start gap-2 p-2 ${members2active && !noTeam2Members ? "pulse-border-green rounded-2xl" : "border-5 border-transparent"}`}>
         {team_2_members && team_2_members.length > 0 ? (
-          <div className='grid grid-cols-1 md:grid-cols-2 gap-4 items-center justify-start'>
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-4 items-center justify-center'>
             {team_2_members.map((member) => (
               <div className="flex flex-2"  key={member.id}>
                 <div className={`flex flex-col items-center justify-center gap-2 ${members2active ? "hover:bg-gray-300 cursor-pointer" :""}  rounded-2xl p-2`}
