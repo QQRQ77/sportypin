55eae618fc7fdf26b077e5a00a99bb32afe8beb4
QQRQ77 <tomaszkokot@o2.pl>
2026-05-05 20:40:50 +0200

    match handball page - score board - subtract action - v3

--- src/components/events/HandBallGame.tsx
+++ src/components/events/HandBallGame.tsx
@@ -60,7 +60,7 @@ const HandBallGame: React.FC<HandBallGameProps> = ({ isUserCreator = false, matc
       }
 
       console.log("gameSignals.score1: ", gameSignals.score1, "prevScore1: ", prevScore1);
-      if (gameSignals.score1 < prevScore1 && gameSignals.scorer1 !== "") {
+      if (gameSignals.score1 < prevScore1) {
         console.log("Score 1 decreased");
         setPrevScore1(gameSignals.score1);
         setGameSignals((prevSignals) => ({ ...prevSignals, scorer1: "" }))  ;
