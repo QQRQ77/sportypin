800b9e0ec37ea8afe4eb3c2b206fe7432e942143
QQRQ77 <tomaszkokot@o2.pl>
2026-05-05 21:59:21 +0200

    match handball page - score board - player stats - gap-2

--- src/components/events/Handball/HandBallPlayerStatsDisplay.tsx
+++ src/components/events/Handball/HandBallPlayerStatsDisplay.tsx
@@ -18,7 +18,7 @@ const HandBallPlayerStatsDisplay: React.FC<HandBallPlayerStatsDisplayProps> = ({
 
 }) => {
   return (
-    <div className='w-14 flex flex-col items-center justify-start border'>
+    <div className='w-14 flex flex-col items-center justify-start gap-2'>
       <div className='w-full h-8 flex flex-2 items-center'>{goals > 0 && <><div className='w-8 flex justify-center items-center'><PiSoccerBallLight size={24} /></div><div className='w-6 text-green-700 font-bold'>{goals}</div></>}</div>
       <div className='w-full h-8 flex flex-2 items-center'>{penalties > 0 && <><div className='w-8 flex justify-center items-center'><PiNumberTwoFill size={24} /></div><div className='w-6 font-bold'>{penalties}</div></>}</div>
       <div className='w-full h-8 flex flex-2 items-center justify-center'>{yellowCards > 0 && <><div className='w-8 flex justify-center items-center'><div className="w-4 h-6 bg-yellow-300 rounded"></div></div><div className="w-6 font-bold">{yellowCards}</div></>}</div>
