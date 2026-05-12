2d3219179f8c13c0b325d867c5ca3f456dbc4d1a
QQRQ77 <tomaszkokot@o2.pl>
2026-05-12 20:29:09 +0200

    match handball page - saving scores - v9

--- src/components/events/HandBallGame.tsx
+++ src/components/events/HandBallGame.tsx
@@ -4,7 +4,7 @@ import React, { useEffect } from 'react';
 import { Timer } from "@/components/events/timer";
 import ScoreBoard from "@/components/events/ScoreBoard";
 import MatchTeamsMembers from "@/components/events/teamsMembers";
-import { saveHarmonogramItem, saveHarmonogramItemTeamPlayers } from '@/lib/events.actions';
+import { saveHarmonogramItemTeamPlayers } from '@/lib/events.actions';
 import { EventTeamMemberType, HarmonogramItem } from '@/types';
 
 interface HandBallGameProps {
@@ -66,7 +66,7 @@ const HandBallGame: React.FC<HandBallGameProps> = (
           const teamOne = team_1.map((member) => (member.id === gameSignals.scorer1 ? { ...member, goals: (member.goals || 0) + 1 } : member))
           setTeam_1(teamOne);
           await saveHarmonogramItemTeamPlayers(eventId, itemData?.id, 1, teamOne);
-          await saveHarmonogramItem(eventId, itemData?.id, {...itemData, team_1_score: itemData.team_1_score ? itemData?.team_1_score + 1 : 1});
+          // await saveHarmonogramItem(eventId, itemData?.id, {...itemData, team_1_score: itemData.team_1_score ? itemData?.team_1_score + 1 : 1});
         }
         setPrevScore1(gameSignals.score1);
         setGameSignals((prevSignals) => ({ ...prevSignals, scorer1: "" }))  ;
