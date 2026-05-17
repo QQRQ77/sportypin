1015465dd831933ce251f2944b46b4f3366e4afc
QQRQ77 <tomaszkokot@o2.pl>
2026-05-17 19:35:50 +0200

    match handball page - game transmission - v3

--- src/components/events/Handball/HandballGameTransmission.tsx
+++ src/components/events/Handball/HandballGameTransmission.tsx
@@ -10,7 +10,9 @@ const HandballGameTransmission: React.FC<HandballGameTransmissionProps> = ({
   gameTransmissionItems,
 }) => {
   return (
-    <section>
+    <section className='flex flex-col border rounded-md bg-white shadow-md'>
+      <div className='flex flex-col flex-2'></div>
+      <div className='bg-orange-600 text-white text-xl p-2 rounded-full border-2 border-white'>Start meczu</div>
       <JSONviewer data={gameTransmissionItems} />
     </section>
   );
