c71a490a58905a2417f4e0f66e5637dba329f8c6
QQRQ77 <tomaszkokot@o2.pl>
2026-05-17 20:38:24 +0200

    match handball page - game transmission - v8

--- src/components/events/Handball/TransmissionSingleItem.tsx
+++ src/components/events/Handball/TransmissionSingleItem.tsx
@@ -13,16 +13,17 @@ const TransmissionSingleItem: React.FC<TransmissionSingleItemProps> = ({
 
   return (
     <div className="transmission-single-item">
-    {transmissionItem.eventType === "goal" && (
-      <div className='w-full flex flex-2 items-center'>
-        <div className='flex justify-center items-center'><PiSoccerBallLight size={24}/> {`${transmissionItem.teamName || ""}`}</div> 
-      </div>
-    )}
-    {transmissionItem.eventType === "penalty" && (
-      <div className='w-full h-8 flex flex-2 items-center'>
-        <div className='w-8 flex justify-center items-center'><PiNumberTwoFill size={24} /></div>
-      </div>
-    )}
+      {transmissionItem.eventType === "goal" && (
+        <div className='w-full flex flex-2 items-center'>
+          <div className='flex justify-center items-center border-l-2 border-orange-900'><PiSoccerBallLight size={24}/> {`${transmissionItem.teamName || ""}`}</div> 
+        </div>
+      )}
+      {transmissionItem.eventType === "penalty" && (
+        <div className='w-full flex flex-2 items-center'>
+          <div className='flex justify-center items-center'><PiNumberTwoFill size={24} /></div>
+        </div>
+      )}
+      <div className='w-full h-8 border-l-2 border-orange-900'></div>
     </div>
   );
 };
