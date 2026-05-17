e3e53fbae7aa1918ea6a820b71404c3affa9585d
QQRQ77 <tomaszkokot@o2.pl>
2026-05-17 21:18:14 +0200

    match handball page - game transmission - v10

--- src/components/events/Handball/TransmissionSingleItem.tsx
+++ src/components/events/Handball/TransmissionSingleItem.tsx
@@ -15,12 +15,34 @@ const TransmissionSingleItem: React.FC<TransmissionSingleItemProps> = ({
     <div className="transmission-single-item">
       {transmissionItem.eventType === "goal" && (
         <div className='w-full flex flex-2 items-center'>
-          <div className='flex justify-center items-center border-l-2 border-orange-900'><PiSoccerBallLight size={24}/> {`${transmissionItem.teamName || ""}`}</div> 
+          <div className='flex justify-center items-center border-l-2 border-orange-900'>
+            <PiSoccerBallLight size={24}/> 
+            {`${transmissionItem.teamName || ""}`}
+          </div> 
         </div>
       )}
       {transmissionItem.eventType === "penalty" && (
         <div className='w-full flex flex-2 items-center'>
-          <div className='flex justify-center items-center'><PiNumberTwoFill size={24} /></div>
+          <div className='flex justify-center items-center border-l-2 border-orange-900'>
+            <PiNumberTwoFill size={24} /> 
+            {`${transmissionItem.teamName || ""}`}
+          </div>
+        </div>
+      )}
+      {transmissionItem.eventType === "redCard  " && (
+        <div className='w-full flex flex-2 items-center'>
+          <div className='flex justify-center items-center border-l-2 border-orange-900'>
+            <div className="w-4 h-6 bg-red-500 rounded"></div>
+            {`${transmissionItem.teamName || ""}`}
+          </div> 
+        </div>
+      )}
+      {transmissionItem.eventType === "yellowCard" && (
+        <div className='w-full flex flex-2 items-center'>
+          <div className='flex justify-center items-center border-l-2 border-orange-900'>
+            <div className="w-4 h-6 bg-yellow-300 rounded"></div>
+            {`${transmissionItem.teamName || ""}`}
+          </div> 
         </div>
       )}
       <div className='w-full h-8 border-l-2 border-orange-900'></div>
