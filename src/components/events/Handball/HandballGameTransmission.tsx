184b85c66d3b5e2ab6d70f462b1a61ea623c71d1
QQRQ77 <tomaszkokot@o2.pl>
2026-05-17 19:46:56 +0200

    match handball page - game transmission - v4

--- src/components/events/Handball/HandballGameTransmission.tsx
+++ src/components/events/Handball/HandballGameTransmission.tsx
@@ -1,6 +1,7 @@
 import JSONviewer from '@/components/utils/JSONviewer';
 import { GameTransmissionItem } from '@/types';
 import React from 'react';
+import TransmissionSingleItem from './TransmissionSingleItem';
 
 interface HandballGameTransmissionProps {
   gameTransmissionItems?: GameTransmissionItem[];
@@ -9,10 +10,19 @@ interface HandballGameTransmissionProps {
 const HandballGameTransmission: React.FC<HandballGameTransmissionProps> = ({
   gameTransmissionItems,
 }) => {
+
   return (
-    <section className='flex flex-col border rounded-md bg-white shadow-md'>
-      <div className='flex flex-col flex-2'></div>
-      <div className='bg-orange-600 text-white text-xl p-2 rounded-full border-2 border-white'>Start meczu</div>
+    <section className='flex flex-col'>
+      <div className='flex flex-col'>
+      {gameTransmissionItems && gameTransmissionItems.length > 0 ? (
+        gameTransmissionItems.map((item) => (
+          <TransmissionSingleItem key={item.id} transmissionItem={item} />
+        ))
+      ) : (
+        <div className='text-gray-500'>Brak transmisji</div>
+      )}
+      </div>
+      <div className='bg-orange-600 text-white text-xl p-2 rounded-full border-2 border-orange-900'>Start meczu</div>
       <JSONviewer data={gameTransmissionItems} />
     </section>
   );
---
+++ src/components/events/Handball/TransmissionSingleItem.tsx
@@ -0,0 +1,30 @@
+import { GameTransmissionItem } from '@/types';
+import React from 'react';
+import { PiSoccerBallLight } from "react-icons/pi";
+import { PiNumberTwoFill } from "react-icons/pi";
+
+interface TransmissionSingleItemProps {
+  transmissionItem: GameTransmissionItem
+}
+
+const TransmissionSingleItem: React.FC<TransmissionSingleItemProps> = ({
+  transmissionItem,
+}) => {
+
+  return (
+    <div className="transmission-single-item">
+    {transmissionItem.eventType === "goal" && (
+      <div className='w-full h-8 flex flex-2 items-center'>
+        <div className='w-8 flex justify-center items-center'><PiSoccerBallLight size={24} /></div> 
+      </div>
+    )}
+    {transmissionItem.eventType === "penalty" && (
+      <div className='w-full h-8 flex flex-2 items-center'>
+        <div className='w-8 flex justify-center items-center'><PiNumberTwoFill size={24} /></div>
+      </div>
+    )}
+    </div>
+  );
+};
+
+export default TransmissionSingleItem;
