f0e4adfaf61f802a77f5e845b55e5d2f53a8bbc1
QQRQ77 <tomaszkokot@o2.pl>
2026-05-18 17:57:22 +0200

    match handball page - game transmission - v15

--- src/components/events/Handball/TransmissionSingleItem.tsx
+++ src/components/events/Handball/TransmissionSingleItem.tsx
@@ -21,8 +21,8 @@ const TransmissionSingleItem: React.FC<TransmissionSingleItemProps> = ({
     <div className="transmission-single-item w-full flex flex-col flex-2 items-center justify-center gap-1">
       <div className={`${transmissionItem.team === 1 ? "flex justify-end order-1" : "flex justify-start order-2"} w-full`}>
         {transmissionItem.eventType === "goal" && (
-          <div className='w-full flex flex-2 items-center gap-2'>
-            <div className='flex justify-center items-center border-l-2 border-orange-900'>          
+          <div className='w-full flex items-center'>
+            <div className='flex justify-center items-center border-l-2 border-orange-900 gap-2'>          
               <div className='text-sm text-gray-700 w-10'>{formatTime(transmissionItem?.time || 0)}</div>
               <PiSoccerBallLight size={24}/> 
               {`${transmissionItem.teamName || ""}`}
@@ -30,8 +30,8 @@ const TransmissionSingleItem: React.FC<TransmissionSingleItemProps> = ({
           </div>
         )}
         {transmissionItem.eventType === "penalty" && (
-          <div className='w-full flex flex-2 items-center'>
-            <div className='flex justify-center items-center border-l-2 border-orange-900'>
+          <div className='w-full flex items-center'>
+            <div className='flex justify-center items-center border-l-2 border-orange-900 gap-2'>
               <div className='text-sm text-gray-700 w-10'>{formatTime(transmissionItem?.time || 0)}</div>
               <PiNumberTwoFill size={24} /> 
               {`${transmissionItem.teamName || ""}`}
@@ -39,8 +39,8 @@ const TransmissionSingleItem: React.FC<TransmissionSingleItemProps> = ({
           </div>
         )}
         {transmissionItem.eventType === "redCard" && (
-          <div className='w-full flex flex-2 items-center'>
-            <div className='flex justify-center items-center border-l-2 border-orange-900'>
+          <div className='w-full flex items-center'>
+            <div className='flex justify-center items-center border-l-2 border-orange-900 gap-2'>
               <div className='text-sm text-gray-700 w-10'>{formatTime(transmissionItem?.time || 0)}</div>
               <div className='w-6 flex justify-center'>
                 <div className="w-4 h-6 bg-red-500 rounded"></div>
@@ -50,8 +50,8 @@ const TransmissionSingleItem: React.FC<TransmissionSingleItemProps> = ({
           </div>
         )}
         {transmissionItem.eventType === "yellowCard" && (
-          <div className='w-full flex flex-2 items-center'>
-            <div className='flex justify-center items-center border-l-2 border-orange-900'>
+          <div className='w-full flex items-center'>
+            <div className='flex justify-center items-center border-l-2 border-orange-900 gap-2'>
               <div className='text-sm text-gray-700 w-10'>{formatTime(transmissionItem?.time || 0)}</div>
               <div className='w-6 flex justify-center'>
                 <div className="w-4 h-6 bg-yellow-300 rounded"></div>
@@ -60,8 +60,9 @@ const TransmissionSingleItem: React.FC<TransmissionSingleItemProps> = ({
             </div> 
           </div>
         )}
-        <div className='w-full h-8 border-l-2 border-orange-900'></div>
+        <div className='w-full h-8 border-l-2 border-orange-900'>TEST</div>
       </div>
+      <div className="w-full h-8 border-l-2 border-orange-900"></div>
     </div>
   );
 };
