e3242d0d9bac19478310f29fb138501e2d992d65
QQRQ77 <tomaszkokot@o2.pl>
2026-05-19 20:59:17 +0200

    match handball page - showing score in game review - v3

--- src/components/events/Handball/TransmissionSingleItem.tsx
+++ src/components/events/Handball/TransmissionSingleItem.tsx
@@ -68,7 +68,7 @@ const TransmissionSingleItem: React.FC<TransmissionSingleItemProps> = ({
     <div className="transmission-single-item w-full flex items-center justify-center">
       <div className="w-1/2 relative flex justify-end border-r-4 border-orange-900 pr-4">
         {transmissionItem.eventType === "goal" ? 
-        <div className="absolute w-20 -right-8 top-1/2 h-10 -translate-y-1/2 border-4 border-orange-900 bg-white rounded-full text-center">{transmissionItem.score || ""}</div>
+        <div className="absolute flex items-center justify-center w-18 -right-8 top-1/2 h-8 -translate-y-1/2 border-4 border-orange-900 bg-white rounded-full font-semibold">{transmissionItem.score || ""}</div>
         : <div className="absolute -right-2.5 top-1/2 h-4 w-4 -translate-y-1/2 border-4 border-orange-900 bg-white rounded-full" />
  }
         {transmissionItem.team === 1 ? renderEvent() : <div className='w-full h-10' />}
