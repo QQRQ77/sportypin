b0533314161116bf0c43a92a5bb8b10508bae7b3
QQRQ77 <tomaszkokot@o2.pl>
2026-06-14 21:06:25 +0200

    event page - game settings

---
+++ src/components/events/eventSettings.tsx
@@ -0,0 +1,15 @@
+
+interface EventSettingsProps {
+  
+};
+
+const EventSettings: React.FC<EventSettingsProps> = () => {
+
+  return (
+    <div className="">
+      
+    </div>
+  );
+};
+
+export default EventSettings;
