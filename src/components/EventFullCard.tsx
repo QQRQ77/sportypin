f5639f8a8e67ab0cda86e71b74a208f5d6eb3558
QQRQ77 <tomaszkokot@o2.pl>
2026-06-14 21:39:03 +0200

    event page - game settings - v5

--- src/components/EventFullCard.tsx
+++ src/components/EventFullCard.tsx
@@ -220,7 +220,7 @@ export default function EventCard({ event, isUserFollowing = false, isUserCreato
               }
             </div>
             <div className="w-full mb-2 flex flex-col justify-center items-right">
-                <h2>Ustawienia dla kategorii: <span className="font-bold">master M</span></h2>
+                <h2 className="text-lg mb-4">Ustawienia dla kategorii: <span className="font-bold text-xl">master M</span></h2>
                 <h2 className="text-lg">Podział czasu gry: <span className="font-bold">2 połowy</span> x <span className="font-bold">30 minut</span>; przerwa: <span className="font-bold">10 minut</span></h2>
                 <h2 className="text-lg">Punktacja: <span className="font-bold">3 punkty</span> za zwycięstwo, <span className="font-bold">1 punkt</span> za remis, <span className="font-bold">0 punktów</span> za porażkę</h2>
                 <h2 className="text-lg">Kary: </h2>
