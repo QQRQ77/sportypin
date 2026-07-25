b557994054e027cc2f9387fbc9b93ad0f4e9e6b0
QQRQ77 <tomaszkokot@o2.pl>
2026-07-25 17:25:55 +0200

    event page - game transmission - penalty timer - <SinglePenaltyTimer/> - timer running

--- src/components/events/Handball/SinglePenaltyTimer.tsx
+++ src/components/events/Handball/SinglePenaltyTimer.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 type SinglePenaltyTimerProps = {
   penalty: { playerId: string; playerNumber: string | number; time: number; teamNumber: number };
   penaltyTimeSeconds?: number;
+  isMainClockRunning?: boolean;
 };
 
 
@@ -12,13 +13,42 @@ const formatTime = (totalSeconds: number): string => {
     return `${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
   };
 
-const SinglePenaltyTimer: React.FC<SinglePenaltyTimerProps> = ({ penalty, penaltyTimeSeconds = 0 }) => {
+const SinglePenaltyTimer: React.FC<SinglePenaltyTimerProps> = ({ penalty, penaltyTimeSeconds = 0, isMainClockRunning = true }) => {
+  
+  const [penaltySeconds, setPenaltySeconds] = useState(penaltyTimeSeconds);
+  const [isPenaltyRunning, setIsPenaltyRunning] = useState(isMainClockRunning && penaltySeconds > 0);
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+
+    if (isPenaltyRunning && penaltySeconds > 0) {
+      interval = setInterval(() => {
+        setPenaltySeconds((prev) => {
+          const nextSecond = prev - 1;
+          if (nextSecond === 0) {
+            setIsPenaltyRunning(false);
+          }
+          return nextSecond;
+        });
+      }, 1000);
+    } else if (penaltySeconds === 0) {
+      setIsPenaltyRunning(false);
+    }
+
+    return () => clearInterval(interval);
+  }, [ isPenaltyRunning, penaltySeconds ]);  
+  
   return (
     <div className="single-penalty-timer flex items-center gap-2">
-      <h3 className="text-lg font-bold">#{penalty.playerNumber}</h3>
-      <p>{formatTime(penalty.time)}</p>
-      <p className="text-lg font-medium mx-3">-</p>
-      <p>{formatTime(penalty.time + penaltyTimeSeconds)}</p>
+      <h3 className='text-lg font-bold h-max'>#{penalty.playerNumber}</h3>
+      <div className="flex flex-col justify-center items-center gap-2">
+        <div className='flex items-center gap-2'>
+          <p>{formatTime(penalty.time)}</p>
+          <p>-</p>
+          <p>{formatTime(penalty.time + penaltyTimeSeconds)}</p>
+        </div>
+        <div className='font-bold text-lg'>{formatTime(penaltySeconds)}</div>
+      </div>
     </div>
   );
 };
