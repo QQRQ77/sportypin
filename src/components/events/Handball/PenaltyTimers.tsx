96b138245220a93a6835d758d525a1204b7ffaf6
QQRQ77 <tomaszkokot@o2.pl>
2026-07-27 20:52:03 +0200

    event page - game transmission - penalty timer - <SinglePenaltyTimer/> - delete penalty - v2

--- src/components/events/Handball/PenaltyTimers.tsx
+++ src/components/events/Handball/PenaltyTimers.tsx
@@ -11,32 +11,32 @@ type PenaltyTimersProps = {
 
 const PenaltyTimers: React.FC<PenaltyTimersProps> = ({penaltyTable, penaltyTimeSeconds = 0}) => {
 
-  const penaltyForTeam1 = penaltyTable?.filter(penalty => penalty.teamNumber === 1);
-  const penaltyForTeam2 = penaltyTable?.filter(penalty => penalty.teamNumber === 2);
+  const [penaltyForTeam1, setPenaltyForTeam1] = React.useState<{ playerId: string; playerNumber: string | number; time: number; teamNumber: number }[] | undefined>(penaltyTable?.filter(penalty => penalty.teamNumber === 1));
+  const [penaltyForTeam2, setPenaltyForTeam2] = React.useState<{ playerId: string; playerNumber: string | number; time: number; teamNumber: number }[] | undefined>(penaltyTable?.filter(penalty => penalty.teamNumber === 2));
 
   if (!penaltyTable || penaltyTable.length === 0) return (<></>);
 
   return (
-    <div className="penalty-timers w-full flex flex-col justify-center items-start gap-10">
+    <div className="penalty-timers w-full flex flex-col justify-center items-center gap-10">
       <h1 className="text-3xl font-bold">Kary:</h1>
       <div className="w-full flex justify-center items-center gap-10">
-        <div className="team-1-penalties w-40 flex flex-col items-start justify-center gap-2">
+        <div className="team-1-penalties w-40 flex flex-col items-start justify-start gap-2">
           {penaltyForTeam1?.map((penalty, index) => (
             <div className="flex gap-2 justify-center items-center" key={index}>            
               <SinglePenaltyTimer penalty={penalty} penaltyTimeSeconds={penaltyTimeSeconds} />
-              <Button variant="outline" size="sm"
-                onClick={() => {}}
+              <Button variant="outline" size="sm" className="text-red-500 hover:text-red-700 transition-colors duration-300 cursor-pointer"
+                onClick={() => {setPenaltyForTeam1(prev => prev?.filter((_, i) => i !== index));}}
               >
                 <FaRegTimesCircle />
               </Button>
             </div>
           ))}
         </div>
-        <div className="team-2-penalties w-40 flex flex-col items-start justify-center gap-2">
+        <div className="team-2-penalties w-40 flex flex-col items-start justify-start gap-2">
           {penaltyForTeam2?.map((penalty, index) => (
             <div className="flex gap-2 justify-center items-center" key={index}>            
               <SinglePenaltyTimer penalty={penalty} penaltyTimeSeconds={penaltyTimeSeconds} />
-              <Button variant="outline" size="sm"
+              <Button variant="outline" size="sm" className="text-red-500 hover:text-red-700 transition-colors duration-300 cursor-pointer"
                 onClick={() => {}}
               >
                 <FaRegTimesCircle />
