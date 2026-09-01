7e5f9495dcb7a12cb9ddc469b44d297d1b7689fc
QQRQ77 <tomaszkokot@o2.pl>
2026-09-01 21:15:32 +0200

    event page - add team members page - v6

--- src/app/(site)/events/[event_id]/teamMembers/[team_Id]/page.tsx
+++ src/app/(site)/events/[event_id]/teamMembers/[team_Id]/page.tsx
@@ -2,16 +2,16 @@ import { EventTeamMembersList } from "@/components/teams/EventTeamMembersList";
 import { getEventParticipants } from "@/lib/events.actions";
 import { Participant } from "@/types";
 
-export default async function teamMembers({ params }: { params: Promise<{ event_id: string, team_id: string }>}) {
+export default async function teamMembers({ params }: { params: Promise<{ event_id: string, team_Id: string }>}) {
 
-const { event_id, team_id } = await params;
+const { event_id, team_Id } = await params;
 
 console.log("event_id:", event_id);
-console.log("team_id:", team_id);
+console.log("team_Id:", team_Id);
 
 const eventParticipants: Participant[] = await getEventParticipants(event_id);
 
-const participant: Participant = eventParticipants.find(p => p.id === team_id)!;
+const participant: Participant = eventParticipants.find(p => p.id === team_Id)!;
 
 return (
   <>
