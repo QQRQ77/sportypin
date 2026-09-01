e7d62fc2197796a5979f6a3d415a0976c8574f48
QQRQ77 <tomaszkokot@o2.pl>
2026-09-01 06:57:49 +0200

    event page - game transmission for viewers - players stats

--- src/components/events/Handball/HandballGameReviewTeamMembers.tsx
+++ src/components/events/Handball/HandballGameReviewTeamMembers.tsx
@@ -1,5 +1,6 @@
 import { EventTeamMemberType } from '@/types';
 import React from 'react';
+import HandBallPlayerStatsDisplay from './HandBallPlayerStatsDisplay';
 
 interface HandballGameReviewTeamMembersProps {
   team_1_members?: EventTeamMemberType[];
@@ -15,12 +16,19 @@ const HandballGameReviewTeamMembers: React.FC<HandballGameReviewTeamMembersProps
   
   return (
     <div className="handball-game-review-team-members w-full lg:w-3/5 flex flex-2 items-start justify-center md:gap-8">
-      <div className="team-1 w-1/2 flex flex-col items-center justify-start gap-2 p-2 border-5 border-transparent">
+      <div className="team-1 w-1/2 flex flex-col items-start justify-start gap-2 p-2 border-5 border-transparent">
         {team_1_members && team_1_members.length > 0 ? (
           <ul className="w-full">
             {team_1_members.map((member) => (
               <li key={member.id} className="text-lg font-medium">
                 {member.name}
+                <HandBallPlayerStatsDisplay
+                  displayDirection="row"
+                  goals={member.goals}
+                  penalties={member.penalties}
+                  yellowCards={member.yellow_cards}
+                  redCards={member.red_cards}
+                />
               </li>
             ))}
           </ul>
@@ -28,12 +36,19 @@ const HandballGameReviewTeamMembers: React.FC<HandballGameReviewTeamMembersProps
           <p className="text-gray-500">Brak członków drużyny</p>
         )}
       </div>
-      <div className="team-2 w-1/2 flex flex-col items-center justify-start gap-2 p-2 border-5 border-transparent">
+      <div className="team-2 w-1/2 flex flex-col items-start justify-start gap-2 p-2 border-5 border-transparent">
         {team_2_members && team_2_members.length > 0 ? (
           <ul className="w-full">
             {team_2_members.map((member) => (
               <li key={member.id} className="text-lg font-medium">
                 {member.name}
+                <HandBallPlayerStatsDisplay
+                  displayDirection="row"
+                  goals={member.goals}
+                  penalties={member.penalties}
+                  yellowCards={member.yellow_cards}
+                  redCards={member.red_cards}
+                />
               </li>
             ))}
           </ul>
