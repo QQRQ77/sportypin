03555cb4c852b0e3c5cc33e6521c0b9f803d6118
QQRQ77 <tomaszkokot@o2.pl>
2026-09-01 11:24:04 +0200

    event page - game transmission for viewers - member icon - v2

--- src/components/events/Handball/HandballGameReviewTeamMembers.tsx
+++ src/components/events/Handball/HandballGameReviewTeamMembers.tsx
@@ -20,7 +20,7 @@ const HandballGameReviewTeamMembers: React.FC<HandballGameReviewTeamMembersProps
           <ul className="w-full">
             {team_1_members.map((member) => (
               <li key={member.id} className="text-lg font-medium flex flex-row items-start justify-start gap-2">
-                <div className="w-72">{member.name} ({member.start_number})</div>
+                <div className="w-72 flex flex-row">{member.name} <IoShirtOutline size={20} /> {member.start_number}</div>
                 <HandBallPlayerStatsDisplay
                   displayDirection="row"
                   goals={member.goals}
@@ -40,7 +40,7 @@ const HandballGameReviewTeamMembers: React.FC<HandballGameReviewTeamMembersProps
           <ul className="w-full">
             {team_2_members.map((member) => (
               <li key={member.id} className="text-lg font-medium flex flex-row items-start justify-start gap-2">
-                <div className="w-72">{member.name} <IoShirtOutline size={16} /> {member.start_number}</div>
+                <div className="w-72 flex flex-row">{member.name} <IoShirtOutline size={20} /> {member.start_number}</div>
                 <HandBallPlayerStatsDisplay
                   displayDirection="row"
                   goals={member.goals}
