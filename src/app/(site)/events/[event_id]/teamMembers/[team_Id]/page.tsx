bfc30ab70ff986a40e9969ac1fe120a7c862a6cb
QQRQ77 <tomaszkokot@o2.pl>
2026-09-01 21:06:50 +0200

    event page - add team members page - v5

--- src/app/(site)/events/[event_id]/teamMembers/[team_Id]/page.tsx
+++ src/app/(site)/events/[event_id]/teamMembers/[team_Id]/page.tsx
@@ -6,6 +6,9 @@ export default async function teamMembers({ params }: { params: Promise<{ event_
 
 const { event_id, team_id } = await params;
 
+console.log("event_id:", event_id);
+console.log("team_id:", team_id);
+
 const eventParticipants: Participant[] = await getEventParticipants(event_id);
 
 const participant: Participant = eventParticipants.find(p => p.id === team_id)!;
