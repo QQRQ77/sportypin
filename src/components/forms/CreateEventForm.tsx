49c58c31568f078ee0f324aab8c24c996c798506
QQRQ77 <tomaszkokot@o2.pl>
2026-09-07 20:28:50 +0200

    event page - subcathegories - flex-col

--- src/components/forms/CreateEventForm.tsx
+++ src/components/forms/CreateEventForm.tsx
@@ -383,7 +383,7 @@ export default function CreateEventForm({ eventToEdit }: Props) {
                                   Dodaj
                                 </Button>
                               </div>
-                              <div className="flex flex-wrap gap-2">
+                              <div className="flex flex-col gap-2">
                                 {cathegories.map((cathegory: string, idx: number) => (
                                   <span key={idx} className="flex items-center bg-gray-200 px-2 py-1 rounded">
                                     {cathegory}
